72c8950edfa636ad562f99d0b28168a6c8c77518
joaobonifacio <joaobonifacio@gmail.com>
2025-06-01 20:22:05 +0100

    Version 2: XML file reading

---
+++ FileReaders/XmlFileReader.cs
@@ -0,0 +1,20 @@
+﻿using FileReaderApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FileReaderApp.FileReaders
+{
+    public class XmlFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            var doc = new XmlDocument();
+            doc.Load(path);
+            return doc.OuterXml;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,42 @@
 ﻿using FileReaderApp.FileReaders;
 using FileReaderApp.Interfaces;
+using System;
+using System.IO; 
 
 class Program
 {
     static void Main()
     {
-        string defaultPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Files", "txt", "sample.txt");
+        //Ask user for file type
+        Console.WriteLine("Choose file type:");
+        Console.WriteLine("1 - Text file");
+        Console.WriteLine("2 - XML file");
 
-        Console.Write($"Enter the path to the text file, or press ENTER to use default ({defaultPath}): ");
+        string typeInput = "";
+        while (typeInput != "1" && typeInput != "2")
+        {
+            Console.Write("Enter your choice (1 or 2): ");
+            typeInput = Console.ReadLine()?.Trim();
+        }
+
+        IFileReader reader;
+        string defaultPath;
+
+        if (typeInput == "1")
+        {
+            reader = new TextFileReader();
+            defaultPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Files", "txt", "sample.txt");
+        }
+        else
+        {
+            reader = new XmlFileReader();
+            defaultPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Files", "xml", "sample.xml"); 
+        }
+
+        Console.Write($"Enter the path to the file, or press ENTER to use default ({defaultPath}): ");
         string inputPath = Console.ReadLine();
 
         string filePath = string.IsNullOrWhiteSpace(inputPath) ? defaultPath : inputPath;
-
-        IFileReader reader = new TextFileReader();
 
         try
         {
